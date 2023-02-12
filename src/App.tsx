a306c131d93e543ff42596e9ac7fd3df8a90af9b
sales@merrows.co.uk <sales@merrows.co.uk>
2023-02-12 13:03:51 +0000

    transfer

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import { WalletAdapterNetwork, WalletNotConnectedError } from '@solana/wallet-adapter-base';
+import { Button, Container, Row, Col} from 'react-bootstrap';
 import { ConnectionProvider, WalletProvider, useConnection, useWallet } from '@solana/wallet-adapter-react';
 import { WalletModalProvider, WalletMultiButton } from '@solana/wallet-adapter-react-ui';
 import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
-import { clusterApiUrl, PublicKey } from '@solana/web3.js';
+import { Connection, Keypair, SystemProgram, Transaction, sendAndConfirmTransaction, clusterApiUrl, PublicKey, LAMPORTS_PER_SOL } from '@solana/web3.js';
 import React, { FC, ReactNode, useEffect, useMemo, useState } from 'react';
-
+import {Buffer} from 'buffer';
 require('./App.css');
 require('@solana/wallet-adapter-react-ui/styles.css');
-
+window.Buffer = Buffer;
 const App: FC = () => {
     return (
         <Context>
@@ -54,24 +56,74 @@ const Context: FC<{ children: ReactNode }> = ({ children }) => {
 };
 
 const Content: FC = () => {
+
      const { connection } = useConnection();
-console.log("connecton == ", connection);
-     const  {publicKey}  = useWallet();
-     const [balance, setBalance] = useState<string | null>(null);
+     console.log("connecton == ", connection);
+     const  {publicKey, sendTransaction}  = useWallet();
+     const [balance, setBalance] = useState(0);
+     const [solAddr, setSolAddr] = useState('0x');
+     const [solAmount, setSolAmount] = useState(0);
+     
      const fetchBalance = async () => {
-console.log("publicKey ----", publicKey);
-           //const publicKey1 = new PublicKey(publicKey.publicKey);
+          console.log("publicKey ----", publicKey);
+          //const publicKey1 = new PublicKey(publicKey.publicKey);
           if (publicKey) {
-           const balance1 = await connection.getBalance(publicKey);
-//console.log("publicKey1 ----", publicKey1);
-           console.log("balance == "+ balance1);
-}
+             const balance1 = await connection.getBalance(publicKey);
+             //console.log("publicKey1 ----", publicKey1);
+             const lamportBalance=(balance1/LAMPORTS_PER_SOL);
+             setBalance(lamportBalance);
+             setBalance(lamportBalance);
+             console.log("balance == "+ balance1);
+         } else {
+             setBalance(0);
+         } 
          //  setBalance(balance1);
-      };
+     };
+
+     const sendSol = async () => {
+
+        if (!publicKey) throw new WalletNotConnectedError();
+
+        const {
+            context: { slot: minContextSlot },
+            value: { blockhash, lastValidBlockHeight }
+        } = await connection.getLatestBlockhashAndContext();
+
+        const lamports = await connection.getMinimumBalanceForRentExemption(0);
+
+        console.log("lamports = ", lamports);
+
+        if (publicKey) {
+       //     const fromPublicKeyPr = Keypair.fromPublicKey(Buffer.from(publicKey), 'hex');
+            const fromPublicKey = new PublicKey(publicKey);
+            const toPublicKey = new PublicKey(solAddr);
+            const k1 = Keypair.generate();
+            console.log("Keypair ", Keypair);
+            console.log("Buffer ", Buffer);
+            console.log("publickey ", publicKey);
+            console.log("k1 public key ", k1.publicKey);
+            console.log("k1 ", k1);
+            //console.log("from public key ", fromPublicKey);
+            console.log("from public key ", fromPublicKey);
+            console.log("to public key ", toPublicKey);
+      	    let transaction = new Transaction();
+            transaction.add(
+              SystemProgram.transfer({
+                fromPubkey: fromPublicKey,
+	        toPubkey: toPublicKey,
+	        lamports: solAmount	   
+	      }),
+	    );
+            const signature = await sendTransaction(transaction, connection, {minContextSlot});
+            await connection.confirmTransaction({blockhash, lastValidBlockHeight, signature});
+          //  await sendAndConfirmTransaction(connection, transaction,[ fromPublicKeyPr]); 
+        }
+    }
 
     useEffect(() => {
         fetchBalance();
     }, [connection, publicKey]);
+
      const handleShowBalance = async () => {
        if (!connection) {
          return;
@@ -86,12 +138,39 @@ console.log("publicKey ----", publicKey);
     //   }
     
      };
+            //<WalletMultiButton />
 
     return (
-        <div className="App">
-            <WalletMultiButton />
-            <button onClick={handleShowBalance}>Show Balance</button>
-            {balance != null ? `balance: ${balance}`: 'connecting ...'}
+        <div >
+            <Container>
+               <Row>
+                   <h1 className="text-light"> Connect to a Wallet</h1>
+               </Row>
+               <Row>
+                   <Col xs={6}>
+                       <WalletMultiButton />
+                   </Col>
+               </Row>
+               <Row>
+                   <Col xs={4}>
+                       <Button variant="primary" onClick={handleShowBalance}>Show Wallet Sol Balance</Button>
+                   </Col>
+                   <Col xs={4} className="text-light">{balance != null ? `Sol Balance: ${balance}`: 'connecting ...'}</Col>
+                   <Col xs={4} className="text-light">{balance != null ? `Lamports Per Sol: ${LAMPORTS_PER_SOL}`: 'connecting ...'}</Col>
+               </Row>
+               <Row>
+                   <Col xs={3}>
+                       <Button variant="primary" onClick={sendSol}>Send Sol</Button>
+                   </Col>
+                   <Col xs={3}>
+                       <input placeholder="solAmount" onChange={(e)=>setSolAmount(parseFloat(e.target.value))} />
+                   </Col>
+                   <Col xs={3}>
+                       <input placeholder="wallet address" onChange={(e)=>setSolAddr(e.target.value)} />
+                   </Col>
+                   <Col xs={3} className="text-light">eg 4dGDp3BuTaXiqJwwJhh9abUBBm6hMhRkidttr5N4Cemm</Col>
+               </Row>
+            </Container>       
         </div>
     );
 };
